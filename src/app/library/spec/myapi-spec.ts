6c331d00961e85681525d80d6f985594534be365
Matt Payne <Payne@MattPayne.org>
2018-08-27 20:28:02 -0500

    Removed test with syntax that made sense but kept failing :-(

--- src/app/library/spec/myapi-spec.ts
+++ src/app/library/spec/myapi-spec.ts
@@ -1,19 +1,14 @@
-import {MyApi} from "../src/MyApi";
+import { MyApi } from '../src/MyApi';
 
-describe("MyApi getName function return value", () => {
-    it("Should be defined.", () => {
-        const myapi = new MyApi();
-        expect(myapi.getName()).toBeDefined("The function getName() should be defined.");
-    });
+describe('MyApi getName function return value', () => {
+  it('Should be defined.', () => {
+    const myapi = new MyApi();
+    expect(myapi.getName()).toBeDefined('The function getName() should be defined.');
+  });
 
-    it("Should't return blank.", () => {
-        const myapi = new MyApi();
-        expect(myapi.getName()).not.toMatch("", "The function getName() should return the name.");
-    });
+  it('Should return "MyName"', () => {
+    const myapi = new MyApi();
+    expect(myapi.getName()).toMatch('MyName1');
+  });
 
-    it("Should return 'MyName'" , () => {
-        const myapi = new MyApi();
-        expect(myapi.getName()).toMatch("MyName1");
-    });
-
-} );
+});
