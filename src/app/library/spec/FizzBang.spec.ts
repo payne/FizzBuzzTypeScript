ead4454a913da5b139ca3d2920b02849b7cc8cba
Matt Payne <Payne@MattPayne.org>
2018-09-10 20:26:57 -0500

    generator returns an object now.

--- src/app/library/spec/FizzBang.spec.ts
+++ src/app/library/spec/FizzBang.spec.ts
@@ -1,23 +1,22 @@
-import { FizzBang } from '../src/FizzBang';
+import { FizzBang } from "../src/FizzBang";
 
-describe('FizzBuzz fizzBuzz simple test case', () => {
-  it('Should start at 1 by default.', () => {
+describe("FizzBuzz fizzBuzz simple test case", () => {
+  it("Should start at 1 by default.", () => {
     const fb = new FizzBang();
     const it = fb.fizzBang();
-    expect(it.next().value).toBe(1);
-    expect(it.next().value).toBe(2);
-    expect(it.next().value).toBe('Fizz');
-    expect(it.next().value).toBe(4);
-    expect(it.next().value).toBe('Buzz');
-    expect(it.next().value).toBe('Fizz');
+    expect(it.next().value.out).toBe(1);
+    expect(it.next().value.out).toBe(2);
+    const three = it.next().value;
+    expect(three.out).toBe("Fizz");
+    expect(three.n).toBe(3);
+    expect(it.next().value.out).toBe(4);
+    expect(it.next().value.out).toBe("Buzz");
+    expect(it.next().value.out).toBe("Fizz");
   });
 
-  it('Can start at 15.', () => {
+  it("Can start at 15.", () => {
     const fb = new FizzBang(15);
     const it = fb.fizzBang();
-    expect(it.next().value).toBe('FizzBuzz');
+    expect(it.next().value.out).toBe("FizzBuzz");
   });
-
 });
-
-
--- src/app/library/src/FizzBang.ts
+++ src/app/library/src/FizzBang.ts
@@ -3,21 +3,23 @@ export class FizzBang {
   public constructor(n = 1) {
     this.n = n;
   }
+
   public *fizzBang() {
     while (true) {
       let result: any = this.n;
       if (this.n % 3 === 0) {
-        result = 'Fizz';
+        result = "Fizz";
       }
       if (this.n % 5 === 0) {
         if (result === this.n) {
-          result = 'Buzz';
+          result = "Buzz";
         } else {
-          result += 'Buzz';
+          result += "Buzz";
         }
       }
+      const retvalue = { out: result, n: this.n };
       this.n++;
-      yield result;
+      yield retvalue;
     }
   }
 }
